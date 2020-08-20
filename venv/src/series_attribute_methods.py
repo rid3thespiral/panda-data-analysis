de6a3fd74955cf44f43328ef2c80319bba499290
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-20 14:55:34 +0200

    series attributes and methods, reading csv

--- venv/src/series_attribute_methods.py
+++ venv/src/series_attribute_methods.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pathlib import Path
 
 letters = ['a', 'e', 'i', 'o', 'u']
 numbers = [10,20,30,40,50]
@@ -10,4 +11,16 @@ print(s1, s2)
 print(s1.dtype, s2.values)
 print(s2.dtype, s2.values)
 print(s1.index)
+print(s1.shape, s1.size)
+print(s2.shape, s2.size)
 
+#methods
+s = pd.Series([1.2, 3.5, 2.8, 2.5])
+print(s.max())
+print(s.min())
+print(s.sum())
+print(s.idxmax())
+print(s.idxmin())
+print(s.round())
+p = str(Path(__file__).parents[2])+"/materials/fifa20.csv"
+print(pd.read_csv(p))
