2311596f9afbf518cba7f8403c08f68010f3c158
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-21 08:59:14 +0200

    series attributes and methods, sorting values, various methods

---
+++ venv/src/broadcasting.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pathlib import Path
+
+#adapting dimensions
+
+s1 = pd.Series(data = [1,2,3])
+s2 = pd.Series(data = [2,3,4])
+print(s1+s2)
+
+s3 = pd.Series(data = [4,5,6,9])
+print(s1+s3) #with NaN broadcasting
+
+print(s3 * 5) #element-wise operation
+
