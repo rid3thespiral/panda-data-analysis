cdf927e729f4be8ff2360b07816cdd4bb62a2a1c
rid3thespiral <davidebattaglia1993@gmail.com>
2020-08-27 15:20:18 +0200

    covid19 italy case

--- venv/src/covid19.py
+++ venv/src/covid19.py
@@ -31,9 +31,35 @@ print(top_10)
 
 plt.style.use('ggplot')
 top_10.plot(kind="bar", figsize= (16,6), rot=45)
-#plt.ylabel("Numero di casi", fontsize=12)
-#plt.show()
+plt.ylabel("Numero di casi", fontsize=12)
+plt.show()
 
 covid["% pop colpita"] = (covid["cases"] / covid["popData2018"])*100
 print(covid.head())
 print(covid.groupby(by= "paesi").agg({"cases": "sum", "% pop colpita": "sum"}).nlargest(n=10, columns="% pop colpita"))
+
+#pie chart by countries
+continenti = covid.groupby(by = "continente").agg({"cases":"sum","deaths":"sum"})
+print(continenti)
+
+dimensioni = continenti["cases"].iloc[:5]
+labels=["Africa", "America", "Asia", "Europe", "Oceania"]
+plt.pie(x=dimensioni, labels=labels, autopct="%1.0f%%", radius=0.5)
+plt.show()
+
+#italy covid
+italy = covid.groupby(by="paesi").get_group("Italy").sort_index()
+print(italy)
+
+italy.plot(y=["cases","deaths"], figsize=(18,5))
+plt.ylabel("Numero di casi giornalieri")
+plt.show()
+
+print(italy.nlargest(n=1,columns="cases")) #worst day
+
+italy["accumulati"] = italy["cases"].cumsum()
+print(italy)
+
+italy.plot(y="accumulati", figsize=(16,6))
+plt.ylabel("Numero di casi totali")
+plt.show()
